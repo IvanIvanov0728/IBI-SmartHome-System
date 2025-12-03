c1f34a0b0230ca9d473421e2ccb2faa4fa8262f9
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2025-12-03 10:32:08 +0200

    Add project files.

---
+++ IBI SmartHome System/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IBI_SmartHome_System.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
