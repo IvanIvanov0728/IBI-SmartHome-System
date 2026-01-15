28fa7db03ed041d58a459600d3c65322c7aebf2a
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-01-15 09:17:28 +0200

    Climet Controller

---
+++ IBI SmartHome System/Controllers/ClimateController.cs
@@ -0,0 +1,21 @@
+﻿using IBI_SmartHome_System.Service.ClimateService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IBI_SmartHome_System.Controllers
+{
+	public class ClimateController : Controller
+	{
+		private readonly IClimateService _climateService;
+
+		public ClimateController(IClimateService climateService)
+		{
+			_climateService = climateService;
+		}
+
+		public IActionResult Index()
+		{
+			var viewModel = _climateService.GetClimateViewModel();
+			return View(viewModel);
+		}
+	}
+}
