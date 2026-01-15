791c7850ca15092d8a4b4c55efa24c5bcd22391f
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-01-15 09:18:45 +0200

    DashboardController

---
+++ IBI SmartHome System/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using IBI_SmartHome_System.Service.DashboardService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IBI_SmartHome_System.Controllers
+{
+	public class DashboardController : Controller
+	{
+		private readonly IDashboardService _dashboardService;
+
+		public DashboardController(IDashboardService dashboardService)
+		{
+			_dashboardService = dashboardService;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var dashboardViewModel = await _dashboardService.GetDashboardViewModelAsync();
+			ViewBag.Thermostat = _dashboardService.GetThermostatViewModel();
+			return View(dashboardViewModel);
+		}
+	}
+}
