6a7a651af4e2c16354d538fe060bbbfaad36c063
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-01-15 09:18:08 +0200

    ClimateService

---
+++ IBI SmartHome System.Service/ClimateService/ClimateService.cs
@@ -0,0 +1,35 @@
+﻿using IBI_SmartHome_System.Data;
+using IBI_SmartHome_System.Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Service.ClimateService
+{
+	public class ClimateService : IClimateService
+	{
+		private readonly ApplicationDbContext _context;
+
+		public ClimateService(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public ClimateViewModel GetClimateViewModel()
+		{
+			var viewModel = new ClimateViewModel
+			{
+				Thermostats = _context.Temperature.Select(t => new ThermostatViewModel
+				{
+					Id = t.Id,
+					Name = t.Device.Name,
+					Temperature = t.TemperatureValue,
+					Humidity = t.Humidity
+				}).ToList()
+			};
+			return viewModel;
+		}
+	}
+}
---
+++ IBI SmartHome System.Service/ClimateService/IClimateService.cs
@@ -0,0 +1,14 @@
+﻿using IBI_SmartHome_System.Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Service.ClimateService
+{
+	public interface IClimateService
+	{
+		ClimateViewModel GetClimateViewModel();
+	}
+}
