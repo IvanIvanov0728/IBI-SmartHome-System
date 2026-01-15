61ba1ad9cb7870830ea27a7b0682c8beda90374e
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-01-15 09:22:47 +0200

    Revert "All sherd viwe"
    This reverts commit e92f0618685fdaf036d9984c713ff67431a77a5b.

--- IBI SmartHome System/Program.cs
+++ IBI SmartHome System/Program.cs
@@ -1,8 +1,5 @@
 using IBI_SmartHome_System.Data;
 using IBI_SmartHome_System.Service;
-using IBI_SmartHome_System.Service.ClimateService;
-using IBI_SmartHome_System.Service.DashboardService;
-using IBI_SmartHome_System.Service.LightingService;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,11 +16,6 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddHostedService<MqttService>();
-
-builder.Services.AddHttpClient<WeatherService>();
-builder.Services.AddScoped<IDashboardService, DashboardService>();
-builder.Services.AddScoped<IClimateService, ClimateService>();
-builder.Services.AddScoped<ILightingService, LightingService>();
 
 var app = builder.Build();
 
@@ -48,7 +40,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
