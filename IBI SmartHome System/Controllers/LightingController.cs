8456d40b69cb9507b8cc97aa0a8e30089b7dd5e3
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-01-15 09:21:32 +0200

    Lighting Controller

---
+++ IBI SmartHome System/Controllers/LightingController.cs
@@ -0,0 +1,21 @@
+﻿using IBI_SmartHome_System.Service.LightingService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IBI_SmartHome_System.Controllers
+{
+	public class LightingController : Controller
+	{
+		private readonly ILightingService _lightingService;
+
+		public LightingController(ILightingService lightingService)
+		{
+			_lightingService = lightingService;
+		}
+
+		public IActionResult Index()
+		{
+			var viewModel = _lightingService.GetLightingViewModel();
+			return View(viewModel);
+		}
+	}
+}
