ca3e9f37c13e5f2cd15d70070bbf640151abb797
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-16 12:09:38 +0200

    View models i need for Lighting api respons

---
+++ IBI SmartHome System.Service/Models/RoomViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Service.Models
+{
+	public class RoomViewModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Floor { get; set; }
+		public IEnumerable<DeviceViewModel> Devices { get; set; }
+	}
+}
