659b9345c9801c761772a11cb8df46c216103b03
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-19 10:58:46 +0200

    SceneConfiguration

---
+++ IBI SmartHome System.Data/Configuration/ScenesConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using IBI_SmartHome_System.Data.Entity;
+using IBI_SmartHome_System.Data.Seeding;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Data.Configuration
+{
+	public class ScenesConfiguration : IEntityTypeConfiguration<Scene>
+	{
+		public void Configure(EntityTypeBuilder<Scene> builder)
+		{
+			builder.HasData(SceneSeeder.Seed());
+		}
+	}
+}
