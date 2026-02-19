66b21e9a1b93f8b5e93502891652100ae9879475
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-19 10:58:26 +0200

    Entity Scene

---
+++ IBI SmartHome System.Data/Entity/Scene.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Data.Entity
+{
+	public class Scene
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		public ICollection<SceneAction> SceneActions { get; set; } = new List<SceneAction>();
+	}
+}
