613bcfb277753a907582d74ed8a8eff82cee5e85
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-19 10:50:30 +0200

    ClimateShedule Configuration

---
+++ IBI SmartHome System.Data/Configuration/ClimateScheduleConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using IBI_SmartHome_System.Data.Entity;
+using IBI_SmartHome_System.Data.Seeding;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Data.Configuration
+{
+	public class ClimateScheduleConfiguration : IEntityTypeConfiguration<ClimateSchedule>
+	{
+		public void Configure(EntityTypeBuilder<ClimateSchedule> builder)
+		{
+			builder.HasData(ClimateScheduleSeeder.Seed());
+		}
+	}
+}
