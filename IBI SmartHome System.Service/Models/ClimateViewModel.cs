3d958662b079c328a6a0705253e726b4e0e3d4bd
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-19 11:07:42 +0200

    Add two props

--- IBI SmartHome System.Service/Models/ClimateViewModel.cs
+++ IBI SmartHome System.Service/Models/ClimateViewModel.cs
@@ -8,6 +8,8 @@ namespace IBI_SmartHome_System.Service.Models
 {
 	public class ClimateViewModel
 	{
+		public double CurrentTemperature { get; set; }
+		public int TargetTemperature { get; set; }
 		public List<ThermostatViewModel> Thermostats { get; set; }
 	}
 }
