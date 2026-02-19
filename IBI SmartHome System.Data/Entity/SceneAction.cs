35f31ab2afb12683325b0742063fc740deb975ed
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-19 10:59:13 +0200

    Entity SceneAction

---
+++ IBI SmartHome System.Data/Entity/SceneAction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace IBI_SmartHome_System.Data.Entity
+{
+	public class SceneAction
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public int SceneId { get; set; }
+
+		[ForeignKey("SceneId")]
+		public Scene Scene { get; set; }
+
+		[Required]
+		public int DeviceId { get; set; }
+
+		[ForeignKey("DeviceId")]
+		public Device Device { get; set; }
+
+		[Required]
+		public string Property { get; set; } // e.g., "Power", "Brightness", "Color", "TemperatureValue"
+
+		[Required]
+		public string Value { get; set; } // e.g., "false", "80", "Red", "22.5"
+	}
+}
