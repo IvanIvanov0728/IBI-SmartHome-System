79c0a1f7b9032abf6fafb76c73fd6f2d5ca50830
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-19 10:43:26 +0200

    View Model for Schedule

---
+++ IBI SmartHome System.Service/Models/ClimateScheduleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Service.Models
+{
+	public class ClimateScheduleViewModel
+	{
+		public int Id { get; set; }
+		public string Day { get; set; }
+		public string Time { get; set; }
+		public string Temp { get; set; }
+		public string Mode { get; set; }
+	}
+}
