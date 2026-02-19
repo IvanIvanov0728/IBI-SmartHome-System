33d8478a383f3fc654010a5ab5b4f8c712999097
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-19 10:49:38 +0200

    ClimateSchedule Entity

---
+++ IBI SmartHome System.Data/Entity/ClimateSchedule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Data.Entity
+{
+	public class ClimateSchedule
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public string Day { get; set; }
+
+		[Required]
+		public string Time { get; set; }
+
+		[Required]
+		public string Temp { get; set; }
+
+		[Required]
+		public string Mode { get; set; }
+	}
+}
