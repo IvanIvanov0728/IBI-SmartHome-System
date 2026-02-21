92dad49cdd3215136f806f7aca600d8dd25cbf6d
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-21 15:59:31 +0200

    Energy Api controller

---
+++ IBI SmartHome System/Controllers/EnergyController.cs
@@ -0,0 +1,24 @@
+﻿using IBI_SmartHome_System.Service.EnergyService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IBI_SmartHome_System.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class EnergyController : ControllerBase
+	{
+		private readonly IEnergyService _energyService;
+
+		public EnergyController(IEnergyService energyService)
+		{
+			_energyService = energyService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Get()
+		{
+			var energyData = await _energyService.GetEnergyDataAsync();
+			return Ok(energyData);
+		}
+	}
+}
