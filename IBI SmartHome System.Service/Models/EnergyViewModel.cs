6127eb7bca1951eaa8fe3c3d36667f56863f5489
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-21 16:02:01 +0200

    model for energy

---
+++ IBI SmartHome System.Service/Models/EnergyViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Service.Models
+{
+	public class EnergyViewModel
+	{
+		public IEnumerable<WeeklyDataPoint> WeeklyData { get; set; }
+		public IEnumerable<HourlyDataPoint> HourlyData { get; set; }
+		public IEnumerable<RoomUsage> RoomData { get; set; }
+		public EnvironmentalImpact EnvironmentalImpact { get; set; }
+		public BatteryStorage BatteryStorage { get; set; }
+	}
+}
