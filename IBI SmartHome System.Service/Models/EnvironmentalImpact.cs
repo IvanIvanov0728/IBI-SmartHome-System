17b4e4b11d487c6252d6336320879d227d86e6a0
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-21 16:01:35 +0200

    model for energy

---
+++ IBI SmartHome System.Service/Models/EnvironmentalImpact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Service.Models
+{
+	public class EnvironmentalImpact
+	{
+		public double Co2Offset { get; set; }
+		public int TreesSaved { get; set; }
+	}
+}
