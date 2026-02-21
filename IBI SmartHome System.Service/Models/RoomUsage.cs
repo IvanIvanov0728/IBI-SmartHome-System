dffd197de02c9356689369bc3a032d8e6dd22ca6
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-21 16:01:52 +0200

    model for energy

---
+++ IBI SmartHome System.Service/Models/RoomUsage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Service.Models
+{
+	public class RoomUsage
+	{
+		public string Room { get; set; }
+		public string Usage { get; set; }
+		public string Color { get; set; }
+		public int Percent { get; set; }
+	}
+}
