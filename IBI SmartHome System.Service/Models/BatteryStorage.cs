71aadfcac989e7e6200e992be03247124c597685
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-21 16:01:28 +0200

    model for energy

---
+++ IBI SmartHome System.Service/Models/BatteryStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Service.Models
+{
+	public class BatteryStorage
+	{
+		public int Percentage { get; set; }
+		public string EstimatedTimeRemaining { get; set; }
+	}
+}
