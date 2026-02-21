c29829e5f00c5b1dc8d4536a3f41310df638ea2d
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-21 16:01:22 +0200

    model for energy

---
+++ IBI SmartHome System.Service/Models/WeeklyDataPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Service.Models
+{
+	public class WeeklyDataPoint
+	{
+		public string Name { get; set; }
+		public int Usage { get; set; }
+		public int Solar { get; set; }
+	}
+}
