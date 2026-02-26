91928b913f9057104c217a3bdc0ed90084c43034
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-26 13:12:21 +0200

    Activity log view model

---
+++ IBI SmartHome System.Service/Models/ActivityLogEntryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Service.Models
+{
+	public class ActivityLogEntryViewModel
+	{
+		public int Id { get; set; }
+		public DateTime Timestamp { get; set; }
+		public string Event { get; set; }
+		public string Type { get; set; } // e.g., "info", "warning", "success"
+	}
+}
