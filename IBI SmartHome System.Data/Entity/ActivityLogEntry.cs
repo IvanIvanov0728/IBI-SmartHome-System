fb7c51ef4223614e832676f53c92e35a3fde0b82
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-26 12:31:59 +0200

    activity log Entity

---
+++ IBI SmartHome System.Data/Entity/ActivityLogEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Data.Entity
+{
+	public class ActivityLogEntry
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public int HouseId { get; set; } // Link to House
+		public House House { get; set; } // Navigation property
+
+		[Required]
+		public DateTime Timestamp { get; set; }
+
+		[Required]
+		public string Event { get; set; } // e.g., "Front Door Locked", "Motion Detected"
+
+		public string? Type { get; set; } // e.g., "info", "warning", "success"
+
+		public int? DeviceId { get; set; }
+		[ForeignKey("DeviceId")]
+		public Device? Device { get; set; }
+	}
+}
