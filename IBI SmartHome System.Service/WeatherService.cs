e8d67e244ac0763c9243a71decea16a6effd593d
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-26 13:11:52 +0200

    Made it so it take latitude and longitude so it is more customasebel

--- IBI SmartHome System.Service/WeatherService.cs
+++ IBI SmartHome System.Service/WeatherService.cs
@@ -16,9 +16,9 @@ namespace IBI_SmartHome_System.Service
 			_httpClient = httpClient;
 		}
 
-		public async Task<WeatherApiResponse> GetWeatherAsync()
+		public async Task<WeatherApiResponse> GetWeatherAsync(double latitude = 42.70, double longitude = 23.32)
 		{
-			var url = "https://api.open-meteo.com/v1/forecast?latitude=42.70&longitude=23.32&current=temperature_2m,relative_humidity_2m,weather_code";
+			var url = $"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,relative_humidity_2m,weather_code";
 			return await _httpClient.GetFromJsonAsync<WeatherApiResponse>(url);
 		}
 	}
