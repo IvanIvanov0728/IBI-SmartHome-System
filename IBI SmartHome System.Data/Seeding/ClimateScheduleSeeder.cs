645d966912864b9df6cbc1fa553f5eae144b48a5
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-26 12:38:07 +0200

    seed HouseId

--- IBI SmartHome System.Data/Seeding/ClimateScheduleSeeder.cs
+++ IBI SmartHome System.Data/Seeding/ClimateScheduleSeeder.cs
@@ -11,13 +11,14 @@ namespace IBI_SmartHome_System.Data.Seeding
 	{
 		public static IEnumerable<ClimateSchedule> Seed()
 		{
+			int adminHouseId = 1;
 			var schedules = new List<ClimateSchedule>
 			{
-				new ClimateSchedule { Id = 1, Day = "Mon-Fri", Time = "07:00 AM", Temp = "72°", Mode = "Heat" },
-				new ClimateSchedule { Id = 2, Day = "Mon-Fri", Time = "09:00 AM", Temp = "68°", Mode = "Eco" },
-				new ClimateSchedule { Id = 3, Day = "Mon-Fri", Time = "05:00 PM", Temp = "72°", Mode = "Heat" },
-				new ClimateSchedule { Id = 4, Day = "Sat-Sun", Time = "08:00 AM", Temp = "72°", Mode = "Heat" },
-				new ClimateSchedule { Id = 5, Day = "Sat-Sun", Time = "11:00 PM", Temp = "67°", Mode = "Sleep" },
+				new ClimateSchedule { Id = 1, Day = "Mon-Fri", Time = "07:00 AM", Temp = "72°", Mode = "Heat", HouseId = adminHouseId },
+				new ClimateSchedule { Id = 2, Day = "Mon-Fri", Time = "09:00 AM", Temp = "68°", Mode = "Eco", HouseId = adminHouseId },
+				new ClimateSchedule { Id = 3, Day = "Mon-Fri", Time = "05:00 PM", Temp = "72°", Mode = "Heat", HouseId = adminHouseId },
+				new ClimateSchedule { Id = 4, Day = "Sat-Sun", Time = "08:00 AM", Temp = "72°", Mode = "Heat", HouseId = adminHouseId },
+				new ClimateSchedule { Id = 5, Day = "Sat-Sun", Time = "11:00 PM", Temp = "67°", Mode = "Sleep", HouseId = adminHouseId },
 			};
 
 			return schedules;
