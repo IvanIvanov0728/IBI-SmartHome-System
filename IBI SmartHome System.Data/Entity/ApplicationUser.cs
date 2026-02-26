c9df2009fe87dde9abcd238b15db5909bd2b032e
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-26 12:36:27 +0200

    Entity for the identity

---
+++ IBI SmartHome System.Data/Entity/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Data.Entity
+{
+	public class ApplicationUser : IdentityUser
+	{
+		// Custom properties for user profile and settings
+		public bool ReceiveNotifications { get; set; } = true;
+		public bool DarkModeEnabled { get; set; } = false;
+
+		// Example: For the "Admin" role status display
+		public string UserRole { get; set; } = "User"; // Will be updated during seeding
+	}
+}
