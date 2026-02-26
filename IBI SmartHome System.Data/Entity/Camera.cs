4b0c200d70065bc85c28ec13e227be6cf3acddbf
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-26 12:31:12 +0200

    camera Entity

---
+++ IBI SmartHome System.Data/Entity/Camera.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Data.Entity
+{
+	public class Camera
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public int HouseId { get; set; } // Link to House
+		public House House { get; set; } // Navigation property
+
+		[Required]
+		public string Name { get; set; } // e.g., "Front Porch Camera"
+
+		[Required]
+		public string StreamUrl { get; set; } // URL to the camera feed (mock for now)
+
+		public bool IsLive { get; set; } = false; // Indicates if it's currently streaming live
+	}
+}
