ec96176efd4bb92e94b9f2fe6c6c7a51014dc0e7
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-26 13:13:32 +0200

    view modesl for settings

---
+++ IBI SmartHome System.Service/Models/UserProfileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Service.Models
+{
+	public class UserProfileViewModel
+	{
+		public string UserName { get; set; }
+		[EmailAddress]
+		public string Email { get; set; }
+		public string UserRole { get; set; }
+	}
+}
---
+++ IBI SmartHome System.Service/Models/UserSettingsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Service.Models
+{
+	public class UserSettingsViewModel
+	{
+		public bool ReceiveNotifications { get; set; }
+		public bool DarkModeEnabled { get; set; }
+	}
+}
