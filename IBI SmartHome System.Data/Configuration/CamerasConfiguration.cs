449b65036a620b4c5f964b01728669a07b4c3151
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-26 12:31:02 +0200

    Camera conf

---
+++ IBI SmartHome System.Data/Configuration/CamerasConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using IBI_SmartHome_System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Data.Configuration
+{
+	public class CamerasConfiguration : IEntityTypeConfiguration<Camera>
+	{
+		public void Configure(EntityTypeBuilder<Camera> builder)
+		{
+			builder.HasData(Seeding.CameraSeeder.Seed());
+		}
+	}
+}
