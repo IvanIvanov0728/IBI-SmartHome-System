8a417b19466d2bc3dd4f0eb4d32c499d3a1693d4
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-02-26 12:32:07 +0200

    activity log conf

---
+++ IBI SmartHome System.Data/Configuration/ActivityLogEntryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using IBI_SmartHome_System.Data.Entity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Data.Configuration
+{
+	public class ActivityLogEntryConfiguration
+	{
+		public void Configure(EntityTypeBuilder<ActivityLogEntry> builder)
+		{
+			builder.HasData(Seeding.ActivityLogEntrySeeder.Seed());
+		}
+	}
+}
