41d67277540ef2855148df7a20ca571faf48d75d
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-03-04 10:25:36 +0200

    Model for activity log reqest

---
+++ IBI SmartHome System/Models/AddActivityLogRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace IBI_SmartHome_System.Models
+{
+	public class AddActivityLogRequest
+	{
+		public string EventDescription { get; set; }
+		public string Type { get; set; }
+		public int? DeviceId { get; set; }
+	}
+}
