a7f943e6ca0fd27e9663427bfaea23de3fed2f8c
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-03-15 16:56:23 +0200

    UITest pages

---
+++ IBI_SmartHome_System.UITests/Pages/DashboardPage.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace IBI_SmartHome_System.UITests.Pages
+{
+	public class DashboardPage
+	{
+		private readonly IWebDriver _driver;
+		private readonly WebDriverWait _wait;
+
+		public DashboardPage(IWebDriver driver)
+		{
+			_driver = driver;
+			_wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+		}
+
+		// Locators
+		private By WelcomeMessage => By.CssSelector("h1.text-4xl");
+		private By WeatherWidget => By.CssSelector(".flex.items-center.gap-2.px-4.py-2.bg-white");
+		private By ActiveRoomName => By.CssSelector("h2.text-3xl.font-display");
+		private By NextRoomButton => By.CssSelector("button .lucide-chevron-right");
+		private By PrevRoomButton => By.CssSelector("button .lucide-chevron-left");
+		private By LogoutButton => By.CssSelector("button .lucide-log-out");
+
+		// Actions
+		public string GetWelcomeMessage()
+		{
+			var element = _wait.Until(ExpectedConditions.ElementIsVisible(WelcomeMessage));
+			return element.Text;
+		}
+
+		public string GetActiveRoomName()
+		{
+			var element = _wait.Until(ExpectedConditions.ElementIsVisible(ActiveRoomName));
+			return element.Text;
+		}
+
+		public void ClickNextRoom()
+		{
+			// Hover might be needed if button is only visible on group-hover
+			var element = _wait.Until(ExpectedConditions.ElementToBeClickable(NextRoomButton));
+			element.Click();
+		}
+
+		public void Logout()
+		{
+			var element = _wait.Until(ExpectedConditions.ElementToBeClickable(LogoutButton));
+			element.Click();
+		}
+
+		public bool IsOnDashboard()
+		{
+			return _driver.Url.EndsWith("/") || _driver.Url.Contains("/dashboard");
+		}
+	}
+}
---
+++ IBI_SmartHome_System.UITests/Pages/LoginPage.cs
@@ -0,0 +1,63 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace IBI_SmartHome_System.UITests.Pages
+{
+	public class LoginPage
+	{
+		private readonly IWebDriver _driver;
+		private readonly WebDriverWait _wait;
+
+		public LoginPage(IWebDriver driver)
+		{
+			_driver = driver;
+			_wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+		}
+
+		// Locators
+		private By EmailInput => By.CssSelector("input[type='email']");
+		private By PasswordInput => By.CssSelector("input[type='password']");
+		private By LoginButton => By.CssSelector("button[type='submit']");
+		private By SignUpLink => By.XPath("//a[contains(text(), 'Sign up')]");
+		private By RememberMeCheckbox => By.Id("remember");
+
+		// Actions
+		public void EnterEmail(string email)
+		{
+			var element = _wait.Until(ExpectedConditions.ElementIsVisible(EmailInput));
+			element.Clear();
+			element.SendKeys(email);
+		}
+
+		public void EnterPassword(string password)
+		{
+			var element = _wait.Until(ExpectedConditions.ElementIsVisible(PasswordInput));
+			element.Clear();
+			element.SendKeys(password);
+		}
+
+		public void ClickLogin()
+		{
+			var element = _wait.Until(ExpectedConditions.ElementToBeClickable(LoginButton));
+			element.Click();
+		}
+
+		public void ClickSignUp()
+		{
+			var element = _wait.Until(ExpectedConditions.ElementToBeClickable(SignUpLink));
+			element.Click();
+		}
+
+		public void ToggleRememberMe()
+		{
+			var element = _wait.Until(ExpectedConditions.ElementToBeClickable(RememberMeCheckbox));
+			element.Click();
+		}
+
+		public bool IsOnLoginPage()
+		{
+			return _driver.Url.Contains("/login") || _driver.PageSource.Contains("Welcome Back");
+		}
+	}
+}
