bddde01b8d829abb7d84d7dd87d090f1649e7698
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-03-15 16:45:27 +0200

    Add initial SignalR hub for smart home real-time features

---
+++ IBI SmartHome System.Service/Hubs/SmartHomeHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBI_SmartHome_System.Service.Hubs
+{
+	public class SmartHomeHub : Hub
+	{
+		
+	}
+}
