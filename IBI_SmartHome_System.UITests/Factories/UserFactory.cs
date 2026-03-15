614e0a03197d7c6b5d5677d2669515a0b84ece76
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-03-15 16:53:46 +0200

    Add UserFactory for generating valid test user credentials
    Created UserFactory.cs in UITests.Factories namespace.
    Provides GetValidUser() method returning a UserModel
    with preset admin email and password for test purposes.

---
+++ IBI_SmartHome_System.UITests/Factories/UserFactory.cs
@@ -0,0 +1,16 @@
+﻿using IBI_SmartHome_System.UITests.Models;
+
+namespace IBI_SmartHome_System.UITests.Factories
+{
+	public static class UserFactory
+	{
+		public static UserModel GetValidUser()
+		{
+			return new UserModel
+			{
+				Email = "admin@smarthome.com",
+				Password = "Password123!"
+			};
+		}
+	}
+}
