3017c4e216cad5812df169ce5ab53ce119286bb2
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-03-15 16:53:08 +0200

    Add BaseTest for Selenium WebDriver setup/teardown
    Introduced an abstract BaseTest class in UITests.Base to manage Selenium Chrome WebDriver lifecycle for UI tests. Includes browser configuration, implicit waits, navigation to default base URL, and NUnit Setup/TearDown methods.

---
+++ IBI_SmartHome_System.UITests/Base/BaseTests.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace IBI_SmartHome_System.UITests.Base
+{
+	public abstract class BaseTest
+	{
+		protected IWebDriver Driver;
+		protected string BaseUrl = "http://localhost:5173"; // Default Vite dev port
+
+		[SetUp]
+		public void Setup()
+		{
+			var options = new ChromeOptions();
+			// options.AddArgument("--headless"); // Uncomment for CI
+			options.AddArgument("--start-maximized");
+			options.AddArgument("--disable-notifications");
+
+			// In a real environment we might need to point to the chromedriver path
+			Driver = new ChromeDriver(options);
+			Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+			Driver.Navigate().GoToUrl(BaseUrl);
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			if (Driver != null)
+			{
+				Driver.Quit();
+				Driver.Dispose();
+			}
+		}
+	}
+}
