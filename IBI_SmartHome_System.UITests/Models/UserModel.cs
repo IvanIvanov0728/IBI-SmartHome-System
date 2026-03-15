e42d024a88aa5a9d0744293b308c67140dbce9b3
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-03-15 16:53:30 +0200

    Add UserModel class for UI test user credentials
    Created UserModel.cs in UITests.Models namespace with Email and Password properties for representing user credentials in UI tests.

---
+++ IBI_SmartHome_System.UITests/Models/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace IBI_SmartHome_System.UITests.Models
+{
+	public class UserModel
+	{
+		public string Email { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+	}
+}
