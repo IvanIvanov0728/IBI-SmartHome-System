a03c5a2cff0be1c3fa70ceec2ceda041531d1c0f
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2026-03-25 08:22:03 +0200

    Core to allow any origin

--- IBI SmartHome System/Program.cs
+++ IBI SmartHome System/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddCors(options =>
 	options.AddPolicy(name: MyAllowSpecificOrigins,
 		policy =>
 		{
-		  policy.WithOrigins("http://localhost:5000", "http://localhost:5173", "http://localhost:3000") // React app's default dev server ports
+		  policy.AllowAnyOrigin() 
 				.AllowAnyHeader()
 				.AllowAnyMethod()
 				.AllowCredentials();
